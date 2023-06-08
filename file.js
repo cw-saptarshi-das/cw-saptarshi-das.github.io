da8ea255da085ef381336ae5c80c4f61401aecf7
Saptarshi Das <saptarshi@carwale.com>
2023-06-08 18:11:11 +0530

    made some mods

--- file.js
+++ file.js
@@ -1,3 +1,13 @@
 function clickHandler() {
-    alert("You just clicked the forbidden button!");
+    let count = 9;
+    alert(`You are ${count} days ahead`);
+    count = countIncrement(count);
+    alert(`You are ${count} days behind`);
+
+}
+
+function countIncrement(count) {
+    document.querySelector("#skill-section > div > div.skill-set-1 > div.skill-item2.skill-item").remove();
+    console.log("reached nested call");
+    return count + 4;
 }
